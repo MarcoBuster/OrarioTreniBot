5c465d3d1cafee6fd202776327a152edfef75399
Marco Aceti <personale@marcoaceti.it>
2017-04-13 13:08:40 +0200

    Created user object with state method

---
+++ OrarioTreniBot/objects/user.py
@@ -0,0 +1,59 @@
+# Copyright (c) 2016-2017 Marco Aceti <dev@marcoaceti.it>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from datetime import datetime as dt
+
+import redis
+
+from OrarioTreniBot import config
+
+r = redis.StrictRedis(host=config.REDIS_HOST, port=config.REDIS_PORT, db=config.REDIS_DB)
+
+
+class User:
+    """
+    User base object
+    """
+    def __init__(self, user):
+        """
+        Create an user object
+        :param user: Telegram's user object
+        """
+        self.id = user.id
+        self.rhash = 'user:' + str(user.id)
+
+        # Redis database
+        r.hset(self.rhash, 'id', user.id)
+        r.hset(self.rhash, 'username', user.username)
+        r.hset(self.rhash, 'last_update', dt.now())
+        if not self.state():
+            r.hset(self.rhash, 'state', 'home')
+
+    def state(self, new_state=None):
+        """
+        Get current user state or set a new user state
+        :param new_state: new state for the user
+        :return: state
+        """
+        if not new_state:
+            return r.hget(self.rhash, 'state')
+
+        r.hset(self.rhash, 'state', new_state)
+        return True
