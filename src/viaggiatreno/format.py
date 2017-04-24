1c978aaa43b79b6d6fc95024771973cbbc430b3f
Marco Aceti <personale@marcoaceti.it>
2017-04-24 21:44:40 +0200

    Optimized imports

--- src/viaggiatreno/format.py
+++ src/viaggiatreno/format.py
@@ -18,9 +18,9 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from . import dateutils
-
 from datetime import datetime
+
+from . import dateutils
 
 
 def formatTrain(raw: dict):
