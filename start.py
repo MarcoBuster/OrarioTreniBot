e7ac79a8392f2fbdf71147f1f82af1f818ad35f2
Marco Aceti <personale@marcoaceti.it>
2017-04-25 19:53:57 +0200

    Ready to merge

--- start.py
+++ start.py
@@ -22,28 +22,29 @@ import os.path as p
 
 
 def gen_config():
-    conf = ('# Copyright (c) 2016-2017 Marco Aceti <dev@marcoaceti.it>\n'
-            '#\n'
-            '# Permission is hereby granted, free of charge, to any person obtaining a copy\n'
-            '# of this software and associated documentation files (the "Software"), to deal\n'
-            '# in the Software without restriction, including without limitation the rights\n'
-            '# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n'
-            '# copies of the Software, and to permit persons to whom the Software is\n'
-            '# furnished to do so, subject to the following conditions:\n'
-            '#\n'
-            '# The above copyright notice and this permission notice shall be included in all\n'
-            '# copies or substantial portions of the Software.\n'
-            '#\n'
-            '# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n'
-            '# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n'
-            '# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n'
-            '# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n'
-            '# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n'
-            '# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n'
-            '# SOFTWARE.\n'
-            '\n'
-            '# -- Redis configuration --\n'
-            '')
+    conf = (
+        '# Copyright (c) 2016-2017 Marco Aceti <dev@marcoaceti.it>'
+        '\n#'
+        '\n# Permission is hereby granted, free of charge, to any person obtaining a copy'
+        '\n# of this software and associated documentation files (the "Software"), to deal'
+        '\n# in the Software without restriction, including without limitation the rights'
+        '\n# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell'
+        '\n# copies of the Software, and to permit persons to whom the Software is'
+        '\n# furnished to do so, subject to the following conditions:'
+        '\n#'
+        '\n# The above copyright notice and this permission notice shall be included in all'
+        '\n# copies or substantial portions of the Software.'
+        '\n#'
+        '\n# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR'
+        '\n# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,'
+        '\n# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE'
+        '\n# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER'
+        '\n# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,'
+        '\n# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE'
+        '\n# SOFTWARE.'
+        '\n'
+        '\n# -- Redis configuration --'
+    )
     rhost = input("Enter the address of the redis server [localhost]: ") or "localhost"
     rport = input("Enter the port of the redis server [6379]: ") or 6379
     assert int(rport), "The redis port must be an integer"
@@ -55,12 +56,14 @@ def gen_config():
 
     conf = conf + "\nREDIS_PORT = " + str(rport)
     conf = conf + "\nREDIS_DB = " + str(rdb)
+
     if rpwd:
         conf = conf + "\nREDIS_PASSWD = \"" + rpwd + "\""
     else:
-        conf = conf + "\nREDIS_PASSWD = None"
-    conf = conf + "\n\n# Telegram Bot API token from t.me/BotFather"
-    conf = conf + "\nBOT_TOKEN = \"" + token + "\"\n"
+        conf += "\nREDIS_PASSWD = None"
+
+    conf += "\n\n# Telegram Bot API token from t.me/BotFather"
+    conf += conf + "\nBOT_TOKEN = \"" + token + "\"\n"
     cfile = open("config.py", "w")
     cfile.write(conf)
     cfile.close()
@@ -69,6 +72,6 @@ def gen_config():
 if __name__ == "__main__":
     if not p.isfile("config.py"):
         gen_config()
-    from src import main
 
+    from src import main
     main.bot.run()
